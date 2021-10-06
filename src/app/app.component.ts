1f20c5a25161ddc16e1b740250a45c25dec3dccc
maglasang32 <angelo.maglasang.20@usjr.edu.ph>
2021-10-07 01:23:51 +0800

    Angular calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Calculator';
+  title = 'Angular-Calculator';
+
+  firstNum!:number;
+  secondNum!:number;
+  result!:number;
+
+  getValue(value:any){
+    this.firstNum = parseInt(value);
+  }
+
+  getValue2(value:any){
+    this.secondNum = parseInt(value);
+  }
+
+  add(){
+    this.result=this.firstNum+this.secondNum
+  }
+
+  sub(){
+    this.result=this.firstNum-this.secondNum
+  }
+
+  multi(){
+    this.result=this.firstNum*this.secondNum
+  }
+
+  div(){
+    this.result=this.firstNum/this.secondNum
+  }
 }
